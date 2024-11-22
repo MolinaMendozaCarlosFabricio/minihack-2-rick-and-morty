3fc8f9c3139d35a1c1daa722e79882c89fa6d241
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-22 12:32:27 -0600

    Inicializando app

---
+++ src/app/characters/card-character/card-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-character',
+  templateUrl: './card-character.component.html',
+  styleUrl: './card-character.component.css'
+})
+export class CardCharacterComponent {
+
+}
---
+++ src/app/characters/characters.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardCharacterComponent } from './card-character/card-character.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardCharacterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharactersModule { }
